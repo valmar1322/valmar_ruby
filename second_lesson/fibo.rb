7d155b9d4740d94c71743069108d3a68d7f9a6e4
Vladimir1223 <vovatheraven@gmail.com>
2019-07-06 23:01:57 +0700

    second lesson v3

--- second_lesson/fibo.rb
+++ second_lesson/fibo.rb
@@ -1,15 +1,10 @@
 
 fibo = [1, 1]
 
-prev, curr = fibo.first, fibo.last
-
-while fibo.last < 100 do
+loop do
   next_val = fibo[-1] + fibo[-2]
-
   break if next_val > 100
-
   fibo << next_val
-
 end
 
 print fibo
--- second_lesson/months.rb
+++ second_lesson/months.rb
@@ -1,6 +1,6 @@
 months = {
   january: 31, february: 28, march: 31,
-  may: 30, april: 31, june: 30,
+  may: 31, april: 30, june: 30,
   july: 31, august: 31, september: 30,
   october: 31, november: 30, december: 31
 }
