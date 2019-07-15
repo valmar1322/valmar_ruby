97a131e51953a7527b23229f86ba78216351cbd9
valmar1322 <vladimir@r70.ru>
2019-07-15 18:33:22 +0700

    check point

--- fourth_lesson/rail_road.rb
+++ fourth_lesson/rail_road.rb
@@ -8,15 +8,115 @@ class RailRoad
     @wagons = []
   end
 
-  def create_station(name)
+  def info
+    puts stations
+    puts ''
+    puts routes
+    puts ''
+    puts trains
+    puts ''
+    puts wagons
+    puts ''
+  end
+
+  def print_menu
+    puts '1  -  Создать поезд'
+    puts '2  -  Создать станцию'
+    puts '3  -  Управление маршрутом(создать, добавить станцию, удалить станцию)'
+    puts '4  -  Назначить маршрут поезду'
+    puts '5  -  Добавить вагон к поезду'
+    puts '6  -  Отцепить вагон от поезда'
+    puts '7  -  Управление движением поезда'
+    puts '8  -  Список станций'
+    puts '9  -  Список поездов'
+    puts '10 -  Выход'
+  end
+
+  def create_train
+    puts 'a - Пассажирский поезд'
+    puts 'b - Грузовой поезд'
+
+    choice = gets.chomp.downcase
+
+    case choice
+    when 'a'
+      type = :passenger
+    when 'b'
+      type = :cargo
+    else
+      type = :passenger
+      puts "Неправильный ввод, по умолчанию выбран: #{type}"
+    end
+
+    puts 'Введите номер(название) поезда: '
+    train_number = gets.chomp
+
+    create_train!(train_number, type)
+  end
+
+  def create_station
+    puts 'Введите название станции: '
+    station_name = gets.chomp
+
+    create_station!(station_name)
+  end
+
+  def manage_routes
+    puts 'a - Создать маршрут'
+    puts 'b - Добавить станцию'
+    puts 'c - Удалить станцию'
+
+    choice = gets.chomp.downcase
+
+    case choice
+    when 'a'
+      stations_info
+      puts 'Выберите начальную станцию: '
+      start_station = stations[gets.to_i]
+
+    when 'b'
+    when 'c'
+    else
+      puts 'Неправильный ввод'
+    end
+  end
+
+  def stations_info
+    stations.each.with_index { |station, index| puts "#{index} - #{station}"}
+  end
+
+  def main_loop
+
+    loop do
+      print_menu
+      input = gets.to_i
+      case input
+      when 1
+        create_train
+      when 2
+        create_station
+      when 3
+        manage_routes
+      when 10
+        break
+      else
+        puts 'Неправильный ввод'
+      end
+    end
+
+  end
+
+  private
+
+  def create_station!(name)
     stations << Station.new(name)
   end
 
-  def create_route(start_point, end_point)
+  def create_route!(start_point, end_point)
     routes << Route.new(start_point, end_point)
   end
 
-  def create_train(number, type)
+  def create_train!(number, type)
     case type
     when :cargo
       trains << CargoTrain.new(number)
@@ -27,7 +127,7 @@ class RailRoad
     end
   end
 
-  def create_wagon(type)
+  def create_wagon!(type)
     case type
     when :cargo
       wagons << CargoWagon.new
@@ -36,39 +136,5 @@ class RailRoad
     else
       "Can't recognize #{type} type"
     end
-  end
-
-  def seed
-    # creating stations
-    (1..5).to_a.each { |i| create_station("station #{i}") }
-
-    # creating route
-    route = create_route(stations.first, stations.last).first
-
-    # filling route
-    stations.each do |station|
-      next if [stations.first, stations.last].include?(station)
-
-      route.add_station(station)
-    end
-    # creating passenger train
-    passenger_train = create_train(533, :passenger).first
-    passenger_train.choose_route(route)
-    # creating passenger wagons
-    (1..10).to_a.each { create_wagon(:passenger) }
-    # filling passenger train
-    wagons.each { |wagon| passenger_train.add_wagon(wagon) }
-
-  end
-
-  def info
-    puts stations
-    puts ''
-    puts routes
-    puts ''
-    puts trains
-    puts ''
-    puts wagons
-    puts ''
   end
 end
